ac106db656d8e55bbe28cafa1f5bd0c1a7914e36
Nicolas Aoki <nicolasaoki@alunos.utfpr.edu.br>
2018-05-14 02:41:02 -0300

    index atualizado

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+    <div class="container spacetop">
+        <div class="row">
+            <div class="col-md-10">
+                <form class="col-md-5" action="index.php" method="post">
+                  <div class="form-group">
+                    <label for="nomeUsuario">Insira o site</label>
+                    <input type="text" class="form-control" name="nome" id="nomeUsuario">
+                  </div>
+                  <button type="submit" name="salva" class="btn btn-primary">Exploit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
